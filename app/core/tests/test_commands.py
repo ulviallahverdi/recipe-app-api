e3fc7b45238340cb16e3e48f24920116ed983475
System Administrator <root@ulvis-iMac.local>
2022-11-08 11:39:46 +0400

    updated_new

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,17 @@
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """ Test Commands . """
+    def test_wait_for_db_ready(self, patched_check):
+        """Test wait for db"""
+        patched_check.return_value = True
+        call_command('wait_for_db')
+        patched_check.assert_called_one_with(database=['default'])
